e80fd7f865424033fcc54e5d7b1a176095c0a641
Sushil Khadka <punksusil.khadka@gmail.com>
2021-10-24 16:31:38 +0545

    Setup Simple JWT authentication

--- miras/settings.py
+++ miras/settings.py
@@ -139,5 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
