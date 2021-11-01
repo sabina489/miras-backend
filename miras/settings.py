edc04242fc70b55875c3d7f8d5aa8b6cd33eb236
shovan777 <raj.shrestha778@gmail.com>
2021-11-01 10:34:13 +0545

    Merge conflicts.

--- miras/settings.py
+++ miras/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
-from corsheaders.defaults import default_headers, default_methods
+from corsheaders.defaults import default_methods, default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'corsheaders',
-    
     'accounts',
 ]
 
@@ -150,13 +149,5 @@ REST_FRAMEWORK = {
     ],
 }
 
-# Cors
-
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_METHODS = list(default_methods) + []
-CORS_ALLOW_HEADERS = list(default_headers) + [
-    'Access-Control-Allow-Origin',
-    'Access-Control-Allow-Headers',
-]
-
-APPEND_SLASH=False
+CORS_ALLOW_METHODS = list(default_methods)
+CORS_ALLOW_HEADERS = list(default_headers)
