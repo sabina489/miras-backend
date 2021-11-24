9f5966b76dd5f662a58d9c8beb05e0dcf7eb5792
shovan777 <raj.shrestha778@gmail.com>
2021-11-24 10:32:51 +0545

    Fix(enrollment): fix typo in app config. Fix(enrollment): Add comma in status. Fix(parts): Remove null, blank fields in many to many.

--- enrollments/apps.py
+++ enrollments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EnrollmentConfig(AppConfig):
+class EnrollmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'enrollments'
