d28a232fabc5df95cbc04a70211809d80648f2c2
shovan777 <raj.shrestha778@gmail.com>
2021-11-28 12:32:49 +0545

    feat(enrollment): Add delete and list views. feat(enrollment): Add delete and list urls.

--- enrollments/api/views.py
+++ enrollments/api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import query
 from rest_framework.generics import (
     CreateAPIView,
     RetrieveAPIView,
@@ -10,18 +11,37 @@ from rest_framework.generics import (
 from rest_framework.permissions import (
     AllowAny,
 )
+from rest_framework.utils import serializer_helpers
 
 from enrollments.models import (
     Enrollment,
 )
 
 from enrollments.api.serializers import(
-    EnrollmentCreateSerializer
+    EnrollmentCreateSerializer,
+    EnrollmentDeleteSerializer,
+    EnrollmentRetrieveSerializer,
 )
 
 User = get_user_model()
+
 
 class EnrollmentCreateAPIView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = EnrollmentCreateSerializer
     queryset = Enrollment.objects.all()
+
+
+class EnrollmentDeleteAPIView(DestroyAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = EnrollmentDeleteSerializer
+    queryset = Enrollment.objects.all()
+
+
+class EnrollmentListAPIView(ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = EnrollmentRetrieveSerializer
+    def get_queryset(self):
+        queryset = Enrollment.objects.all()
+        student_id = self.kwargs['student_id']
+        return queryset.filter(student=student_id)
