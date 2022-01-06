edab8f1944b569ae39503ef01fe493db083c139b
shovan777 <raj.shrestha778@gmail.com>
2022-01-06 12:18:51 +0545

    Activate enrollment on successful payment.

--- enrollments/signals.py
+++ enrollments/signals.py
@@ -1,11 +1,22 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from courses.models import CourseStatus
-from enrollments.api.utils import end_enrollment
+from payments.models import PaymentStatus
+from enrollments.models import ExamStatus
+from enrollments.api.utils import (
+    end_enrollment,
+    activate_enrollment
+)
 
 
 @receiver(post_save, sender='courses.Course')
 def on_course_end(sender, instance, **kwargs):
     if instance.status == CourseStatus.ENDED:
         ended_enrolls = end_enrollment(instance)
-        print('these enrollments have ended : \n', ended_enrolls)
+
+
+@receiver(post_save, sender='payments.OnlinePayment')
+def on_paid_status(sender, instance, **kwargs):
+    if instance.status == PaymentStatus.PAID:
+        activated_enroll = activate_enrollment(instance)
+        print('activated enrollment', activated_enroll)
