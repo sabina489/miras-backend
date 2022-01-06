45d56528370330fff6a8729f2000c66b5c5f133b
shovan777 <raj.shrestha778@gmail.com>
2022-01-06 12:36:27 +0545

    Add price to parts. Check active enrollment in parts. Migrate db changes.

--- part/api/serializers.py
+++ part/api/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 from enrollments.models import EnrollmentStatus
 
 from part.models import Part
-from enrollments.api.utils import count_enrollments
+from enrollments.api.utils import count_enrollments, is_enrolled, is_enrolled_active
 
 
 class PartRetrieveSerializer(serializers.ModelSerializer):
 
     count = serializers.SerializerMethodField()
     is_enrolled = serializers.SerializerMethodField()
+    is_enrolled_active = serializers.SerializerMethodField()
 
     class Meta:
         model = Part
@@ -20,6 +21,7 @@ class PartRetrieveSerializer(serializers.ModelSerializer):
             "price",
             "count",
             "is_enrolled",
+            "is_enrolled_active",
         )
 
     def get_count(self, obj):
@@ -30,21 +32,23 @@ class PartRetrieveSerializer(serializers.ModelSerializer):
         return count_enrollments(obj)
 
     def get_is_enrolled(self, obj):
+        return is_enrolled(obj, self.context["request"].user)
+
+    def get_is_enrolled_active(self, obj):
         """
         Get the enrollment status of the user for the part.
         obj: Part object
         """
-        enrollments = []
-        if self.context["request"].user.is_authenticated:
-            enrollments = obj.enrolls.all().filter(
-                student=self.context['request'].user, status=EnrollmentStatus.ACTIVE)
-        if len(enrollments) > 0:
-            return True
-        return False
+        return is_enrolled_active(obj, self.context["request"].user)
 
 
 class PartSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Part
-        fields = ("id", "name")
+        fields = (
+            "id",
+            "name",
+            "price",
+            "detail",
+            )
