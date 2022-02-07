52a7ea9d7136ad4d72fd82dc97c55d6daad12dbc
Sushil Khadka <punksusil.khadka@gmail.com>
2022-02-07 21:13:05 +0545

    OTP store and send according to aakashsms

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from common.models import OTP
 
-# Register your models here.
+
+class OTPAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'otp', 'otp_expiry',
+                    'error_status', 'result', 'created_on')
+
+
+admin.site.register(OTP, OTPAdmin)
