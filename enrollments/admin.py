f91c28c54f1df86f3027153b5d1c3f0512a926f4
Sushil Khadka <punksusil.khadka@gmail.com>
2022-05-01 14:21:59 +0545

    Send email to selected enrollment users

--- enrollments/admin.py
+++ enrollments/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.shortcuts import render
+from enrollments.forms import EmailForm
 from enrollments.models import (
     Enrollment,
     ExamStatus,
@@ -10,6 +12,10 @@ from exams.models import (
 from payments.admin import OnlinePaymentInline, BankPaymentInline
 from import_export.admin import ExportMixin
 from enrollments.resources import EnrollmentResource
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from common.utils import send_mail_common
+from django.template.response import TemplateResponse
 
 class QuestionStatusInline(admin.StackedInline):
     model = QuestionStatus
@@ -39,8 +45,53 @@ class EnrollmentAdmin(ExportMixin, admin.ModelAdmin):
     def email(self, obj):
         return obj.student.email
 
+    def name(self, obj):
+        return obj.student.get_full_name()
+
+    def send_email(self, request, queryset):
+        opts = self.model._meta
+        app_label = opts.app_label
+        context = {
+            **self.admin_site.each_context(request),
+            'title': "Send Email",
+            'subtitle': None,
+            'object_name': "Enrollment",
+            'object': queryset,
+            'model_count': queryset.count(),
+            'opts': opts,
+            'app_label': app_label,
+            'preserved_filters': self.get_preserved_filters(request),
+            'items': queryset,
+        }
+        if 'apply' in request.POST:  # if user pressed 'apply' on intermediate page
+            # Cool thing is that params will have the same names as in forms.py
+            email_context = {
+                "message": request.POST["message"] ,
+            }
+            subject = request.POST["subject"]
+
+            # Run background task that will send broadcast messages
+            for enroll in queryset:
+                if to := enroll.student.email:
+                    send_mail_common("enrollments/email.html", email_context, [to], subject)
+
+            # Show alert that everything is cool
+            self.message_user(request, f"Message has been send to {queryset.count()} students")
+
+            # Return to previous page
+            return HttpResponseRedirect(request.get_full_path())
+
+        # Create form and pass the data which objects were selected before triggering 'broadcast' action
+        # We create an intermediate page right here 
+        form = EmailForm(initial={'_selected_action': queryset.values_list('id', flat=True)})
+        context['form'] = form
+
+        # We need to create a template of intermediate page with form - but this is really easy
+        return TemplateResponse(request, "enrollments/admin_email.html", context)
+
+
     readonly_fields = ('id',)
-    list_display = ('id', 'student',"email", 'status', 'enrolled_on', 'created_at')
+    list_display = ('id', 'name', 'student',"email", 'status', 'enrolled_on', 'created_at')
     list_filter = ('status',"parts__course", 'parts', 'exams', 'notes')
     search_fields = ['student__phone',]
     inlines = [
@@ -48,6 +99,7 @@ class EnrollmentAdmin(ExportMixin, admin.ModelAdmin):
         OnlinePaymentInline,
         BankPaymentInline,
     ]
+    actions = ['send_email']
     exclude = ('exams',)
 
 
