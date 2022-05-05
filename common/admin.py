0ca469b75383472118c51e6813ecc1130a5feb71
Sushil Khadka <punksusil.khadka@gmail.com>
2022-05-05 22:37:56 +0545

    Make instructor blank true null true:

--- common/admin.py
+++ common/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from common.models import OTP
 
+admin.site.site_header = "Miras Academy"
+admin.site.site_title = "Miras Academy"
+admin.site.index_title = "Miras Academy Dashboard"
+
 
 class OTPAdmin(admin.ModelAdmin):
     list_display = ('phone', 'otp', 'otp_expiry',
